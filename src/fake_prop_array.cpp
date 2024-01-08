3ab86dd55a7ecfd06c714c90dda9d15fedf5647e
adamm08 <adamm@mun.ca>
2024-01-08 20:07:28 -0330

    working with new findMidpoint. work in progress

--- src/fake_prop_array.cpp
+++ src/fake_prop_array.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh;
 
     ros::Publisher pub = nh.advertise<prop_mapper::PropArray>("props", 1000);
-    ros::Publisher task_pub = nh.advertise<task_master::TaskStatus>("task_to_execute", 1000);
+    //ros::Publisher task_pub = nh.advertise<task_master::TaskStatus>("task_to_execute", 1000);
     ros::Rate loop_rate(2);
 
     // TEST PROP SET 1
@@ -52,24 +52,28 @@ int main(int argc, char** argv) {
     red_prop_g1.vector.x = 1;       //
     red_prop_g1.vector.y = 5;       // red prop at (-1.25, 5, 0)
     red_prop_g1.vector.z = 0;       //
+    red_prop_g1.part_of_gate = 0;
 
     prop_mapper::Prop green_prop_g1;
     green_prop_g1.prop_label = "Green Prop";
     green_prop_g1.vector.x = 2;     //
     green_prop_g1.vector.y = 3.5;   // green prop at (1.25, 5, 0)
     green_prop_g1.vector.z = 0;     //
+    green_prop_g1.part_of_gate = 0;
 
     prop_mapper::Prop red_prop_g2;
     red_prop_g2.prop_label = "Red Prop";
     red_prop_g2.vector.x = 2;       //
     red_prop_g2.vector.y = 25;      // red prop at (-1.4, 25, 0)
     red_prop_g2.vector.z = 0;       //
+    red_prop_g2.part_of_gate = 0;
 
     prop_mapper::Prop green_prop_g2;
     green_prop_g2.prop_label = "Green Prop";
     green_prop_g2.vector.x = 3.5;   //
     green_prop_g2.vector.y = 24;    // green prop at (1, 24, 0)
     green_prop_g2.vector.z = 0;     //
+    green_prop_g2.part_of_gate = 0;
 
     prop_mapper::PropArray prop_array;
     prop_array.props.push_back(red_prop_g1);
